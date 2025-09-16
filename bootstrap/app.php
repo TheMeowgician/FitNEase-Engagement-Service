664286edbc6bc3edae538a1bed45e153873da5f6
TheMeowgician <gabsmaglaya@gmail.com>
2025-09-17 03:13:34 +0800

    Register auth.api middleware in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->validateCsrfTokens(except: [
+            'api/*',
+        ]);
+
+        // Register custom API token validation middleware
+        $middleware->alias([
+            'auth.api' => \App\Http\Middleware\ValidateApiToken::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
